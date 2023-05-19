20fc8e635784cabf71b0c91ae5e87b47884df2bb
henos1206 <henostsegai@gmail.com>
2023-05-19 13:55:26 +0100

    Cleaned up the structuring of the API so player names are they first keys. Also fully type hinted & docstringed the functions.

--- app/api_utils.py
+++ app/api_utils.py
@@ -9,78 +9,81 @@ def load_nba_data() -> pd.DataFrame:
     """
     return pd.read_csv("season_stats.csv")
 
-def create_player_bio_dict(player:pd.DataFrame) -> dict:
-    """Formats data that would be present in a player's bio
+
+def create_player_bio_dict(player: pd.DataFrame) -> dict:
+    """Create the bio of a player
 
     Args:
-        player (pd.DataFrame): a single player's details
+        player (pd.DataFrame): the season's stats of a player
 
     Returns:
-        dict: formatted bio data
+        dict: their formatted bio
     """
     return {
-        'player_id':player['rank'],
-        'first_name':player['player'].split(' ')[0],
-        'surname':player['player'].split(' ')[1],
+        'player_id':player['rank'],        
         'position':player['position'],
         'age':player['age'],
         'team':player['team']
-    }
+        }
 
-def player_profile() -> list:
-    """Creates a list of dictionaries containing player bio data
 
-    Returns:
-        list: list of player data
-    """
-    df = load_nba_data()
-    player_df = df.filter(regex = "rank|player|position|age|team")
-    players_bio = [create_player_bio_dict(player) for i,player in player_df.iterrows()]
-    return players_bio
-
-player_profiles = player_profile()
-
-# def player_profiles_by_id(id: int) -> dict:
-#     """Gets the bios of players who have been requested by id
-
-#     Args:
-#         ids (int): player id
-
-#     Returns:
-#         dict: dictionary of the selected players' bio 
-#     """
-#     all_bios = player_profiles
-#     for bio in all_bios:
-#         if bio['player_id'] == id:
-#             return bio
-
-def player_profiles_by_name(first_name: str, surname: str) -> dict:
-    """Gets the bios of players who have been requested by id
+def create_player_defense_dict(player: pd.DataFrame) -> dict:
+    """Formats a player's defensive stats
 
     Args:
-        ids (int): player id
+        player (pd.DataFrame): the season's stats of a player
 
     Returns:
-        dict: dictionary of the selected players' bio
+        dict: their formatted defensive stats
     """
-    all_bios = player_profiles
-    for bio in all_bios:
-        if bio['first_name'].lower() == first_name.lower() and bio['surname'].lower() == surname.lower():
-            return bio
+    return {
+        f"{player['player']}":{
+        'appearances': {
+        'games_played':player['games'],
+        'mins_played':player['mins_played']
+        },
+            'rebounding':{
+                'total_rebounds':player['TRB'],
+                'offensive_rebounds':player['ORB'],
+                'offensive_rebounds':round(player['ORB']/player['games'],2),
+                'defensive_rebounds':player['DRB'],
+                'defensive_rebounds_per_game':round(player['DRB']/player['games'],2)
+            },
+            'steals':{
+                'steals':player['STL'],
+                'steals_per_game':round(player['STL']/player['games'],2)
+            },
+            'blocks':{
+                'blocks':player['BLK'],
+                'blocks_per_game':round(player['BLK']/player['games'],2)
+            },
+            'turnovers':{
+                'turnovers':player['TOV'],
+                'turnovers_per_game':round(player['TOV']/player['games'],2)
+            },
+            'fouls':{
+                'fouls':player['fouls'],
+                'fouls_per_game':round(player['fouls']/player['games'],2)
+            }
+        }
+    }
+
 
 def create_player_offense_dict(player: pd.DataFrame) -> dict:
     """Formats a player's offensive stats
 
     Args:
-        player (pd.DataFrame): all recorded stats for a player
+        player (pd.DataFrame): the season's stats of a player
 
     Returns:
-        dict: formatted stats
+        dict: their formatted offensive stats
     """
     return {
         f"{player['player']}":{
+        'appearances': {
         'games_played':player['games'],
-        'mins_played':player['mins_played'],
+        'mins_played':player['mins_played']
+        },
         'total_points':player['points'],
         'points_per_game':round(player['points']/player['games'],2),
         'field_goals':{
@@ -107,98 +110,173 @@ def create_player_offense_dict(player: pd.DataFrame) -> dict:
         'assists':{
             'total_assists':player['AST'],
             'assists_per_game':round(player['AST']/player['games'],2)
+            }
         }
     }
-}
 
-def player_offense_stats() -> list:
-    """Creates a list of dictionaries containing player shooting/scoring stats
 
-    Returns:
-        list: list of player stats
-    """
-    df = load_nba_data()
-    player_df = df.filter(regex = "player|games|mins_played|FG|FGA|FG%|3P|3PA|3P%|2P|2PA|2P%|eFG%|FT|FTA|FT%|points|AST")
-    players_offense = [create_player_offense_dict(player) for i,player in player_df.iterrows()]
-    return players_offense
-
-players_offense = player_offense_stats()
-
-def create_player_defense_dict(player: pd.DataFrame) -> dict:
-    """Formats a player's defensive stats
+def create_player_dict(player:pd.DataFrame) -> dict:
+    """Collects all the formatted data on a player together
+    to produce their complete profile
 
     Args:
-        player (pd.DataFrame): all recorded stats for a player
+        player (pd.DataFrame): the season's stats of a player
 
     Returns:
-        dict: formatted stats
+        dict: their formatted complete overview for the season
     """
     return {
-        f"{player['player']}":{
-            'games_played':player['games'],
-            'mins_played':player['mins_played'],
-            'rebounding':{
-                'total_rebounds':player['TRB'],
-                'offensive_rebounds':player['ORB'],
-                'offensive_rebounds':round(player['ORB']/player['games'],2),
-                'defensive_rebounds':player['DRB'],
-                'defensive_rebounds_per_game':round(player['DRB']/player['games'],2)
-            },
-            'steals':{
-                'steals':player['STL'],
-                'steals_per_game':round(player['STL']/player['games'],2)
-            },
-            'blocks':{
-                'blocks':player['BLK'],
-                'blocks_per_game':round(player['BLK']/player['games'],2)
-            },
-            'turnovers':{
-                'turnovers':player['TOV'],
-                'turnovers_per_game':round(player['TOV']/player['games'],2)
-            },
-            'fouls':{
-                'fouls':player['fouls'],
-                'fouls_per_game':round(player['fouls']/player['games'],2)
+        player['player']: {
+        'bio': create_player_bio_dict(player), 
+        'offense': create_player_offense_dict(player),
+        'defense': create_player_defense_dict(player),
         }
     }
-}
 
-def player_defense_stats() -> list:
-    """Creates a list of dictionaries containing player defensive stats
+
+def player_profile() -> list:
+    """Collects all the profiles for every player in the NBA 
+    for the 22/23 season and organises them in a list
 
     Returns:
-        list: list of player stats
+        list: the complete list of player profiles for the season
     """
-    df = load_nba_data()
-    player_df = df.filter(regex = "player|games|mins_played|ORB|DRB|TRB|STL|BLK|TOV|fouls")
-    players_defense = [create_player_defense_dict(player) for i,player in player_df.iterrows()]
-    return players_defense  
+    players = load_nba_data()
+    players_bio = [create_player_dict(player) for i,player in players.iterrows()]
+    return players_bio
 
-players_defense = player_defense_stats()
 
-def delete_players(first_name: str, surname: str) -> dict:
-    """Removes the bios of the players that have been selected in the DELETE request
+player_profiles = player_profile()
+
+
+def player_name(player: dict) -> str:
+    """Extracts the player name from the formatted overview
+    of the player's season
 
     Args:
-        id (list): ids of the players to be removed
+        player (dict): formatted season overview of a player
 
     Returns:
-        list: players remaining
+        str: the player's name
+    """
+    return list(player.keys())[0]
+
+
+def name_matches(first_name: str, surname: str, player: dict) -> bool:
+    """Checks whether the name given in a request matches the 
+    name of the player in the API
+
+    Args:
+        first_name (str): first name provided in the request
+        surname (str): surname provided in the request
+        player (dict): name of the player currently being checked
+    Returns: 
+        bool: whether it is true or not that the names match
+    """
+    return player_name(player).lower() == first_name.lower() + ' ' + surname.lower()
+    
+
+def player_profiles_by_name(first_name: str, surname: str) -> dict:
+    """Finds the season overview of the player who's name
+    matches
+
+    Args:
+        first_name (str): the requested player's first name
+        surname (str): the requested player's surname
+    Returns:
+        dict: the season overview of the requested player
     """
     players = player_profiles
-    
+
+    for player in players:
+        if name_matches(first_name, surname, player):
+            return player
+
+
+def delete_players(first_name: str, surname: str) -> dict:
+    """Removes the season overview of the player who's name
+    matches
+
+    Args:
+        first_name (str): the requested player's first name
+        surname (str): the requested player's surname
+
+    Returns:
+        list: the season overview of every other player in the NBA
+    """
+    players = player_profiles
+
     for player in players.copy():
-        if player.get('first_name').lower() == first_name and player.get('surname').lower() == surname:
+        if name_matches(first_name, surname, player):
             players.remove(player)
-    return f"{first_name} {surname}'s bio has been deleted!"
 
-# def change_players_team(id: int, new_player: dict) -> dict:
+    return f"{first_name} {surname}'s stats for the 22/23 season have been deleted!"
 
-#     players = player_profiles
-#     for player in players.copy():
-#         if player.get('player_id') == id:
-            
-#             return updated_player
+
+def player_offense_stats(player_profiles: list) -> list:
+    """Collects just the offensive stats of every active NBA
+    player for the season
+
+    Args: 
+        player_profiles (list): the season overview of every player
+
+    Returns:
+        list: the offensive stats of every player
+    """
+    return [{player_name(player):player.get(player_name(player)).get('offense')} for player in player_profiles]
+
+
+players_offense = player_offense_stats(player_profiles)
+
+
+def player_defense_stats(player_profiles: list) -> list:
+    """Creates a list of dictionaries containing player defensive stats
+
+    Args: 
+        player_profiles (list): the season overview of every player
+    Returns:
+        list: the defensive stats of every player
+    """
+    return [{player_name(player):player.get(player_name(player)).get('defense')} for player in player_profiles]
+
+
+players_defense = player_defense_stats(player_profiles)
+
+
+def player_offense_by_name(first_name: str, surname: str) -> dict:
+    """Finds the season offensive stats of the player who's name
+    matches
+
+    Args:
+        first_name (str): the requested player's first name
+        surname (str): the requested player's surname
+    Returns:
+        dict: the offensive stats of the requested player
+    """
+    players = player_profiles
+
+    for player in players:
+        if name_matches(first_name, surname, player):
+            return player.get(player_name(player)).get('offense')
+
+
+def player_defense_by_name(first_name: str, surname: str) -> dict:
+    """Finds the season defensive stats of the player who's name
+    matches
+
+    Args:
+        first_name (str): the requested player's first name
+        surname (str): the requested player's surname
+    Returns:
+        dict: the defensive stats of the requested player
+    """
+    players = player_profiles
+
+    for player in players:
+        if name_matches(first_name, surname, player):
+            return player.get(player_name(player)).get('defense')
+
+
 
 
     
--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
+import json
+
 from fastapi import FastAPI
 
-from .api_utils import delete_players, player_profiles, player_profiles_by_name
+from .api_utils import (delete_players, player_defense_by_name,
+                        player_offense_by_name, player_profiles,
+                        player_profiles_by_name, players_defense,
+                        players_offense)
 
 app = FastAPI()
 
@@ -10,16 +15,28 @@ async def root():
 
 @app.get('/players')
 async def players():
-    return player_profiles
+    return json.dumps(player_profiles, indent = "")
 
 @app.get('/players/')
 async def player_by_name(first_name: str, surname: str):
     return player_profiles_by_name(first_name,surname)
 
+@app.get('/players/offense')
+async def get_players_offense():
+    return json.dumps(players_offense)
+
+@app.get('/players/offense/')
+async def get_player_offense_by_name(first_name: str, surname: str):
+    return player_offense_by_name(first_name,surname)
+
+@app.get('/players/defense')
+async def get_players_defense():
+    return json.dumps(players_defense)
+
+@app.get('/players/defense/')
+async def get_player_defense_by_name(first_name: str, surname: str):
+    return player_defense_by_name(first_name,surname)
+
 @app.delete('/players/')
 async def remove_player_by_name(first_name: str, surname: str):
     return delete_players(first_name,surname)
-
-# @app.patch('/players/')
-# async def adjust_players_team(team: str):
-#     return change_players_team(team)
